220c7d6cc626694a5140210a4ec5c42ce6a0adf4
moella <moella0@gmail.com>
2022-03-17 21:22:08 +0100

    Diagram toegevoegd en kleine aanpassingen gedaan aan de opdracht

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
     }
 
-
     public static void main(String[] arg) {
         Zwembad z1 = new Zwembad(2.0, 5.5, 1.5);
-        z1.
+
         System.out.println("BREEDTE: " + z1.getBreedte());
         System.out.println("LENGTE: " + z1.getLengte());
         System.out.println("DIEPTE: " + z1.getDiepte());
@@ -21,7 +20,7 @@ public class Main {
         z2.setLengte(100.0);
         z2.setDiepte(2.0);
         double inh = z2.inhoud();
-        System.out.println("GEGEVENS ZWEMBAD: "+z2.toString());
+        System.out.println("GEGEVENS ZWEMBAD: " + z2.toString());
         System.out.println("BEREKENDE INHOUD: " + z2.inhoud());
     }
 }
