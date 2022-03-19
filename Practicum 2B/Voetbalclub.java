568838090a788d99b2eb6c178abd983604340e83
moella <moella0@gmail.com>
2022-03-19 12:41:36 +0100

    comments toegevoegd

--- Practicum 2B/Voetbalclub.java
+++ Practicum 2B/Voetbalclub.java
@@ -1,22 +1,26 @@
 package v1oop.practicum.practicum2b;
 
-
+/*public class voetbal met de private attributen*/
 public class Voetbalclub {
     private String club;
     private int aantalGewonnen;
     private int aantalGelijk;
     private int aantalVerloren;
 
+    /*public voetbalclu string*/
     public Voetbalclub(String cl) {
         club = cl;
     }
+    /*public int aantalPunten met return*/
     public int aantalPunten() {
         return aantalGewonnen * 3 + aantalGelijk;
     }
+    /*public int aantalGespeeld met return*/
     public int aantalGespeeld() {
         return aantalGewonnen + aantalGelijk + aantalVerloren;
     }
 
+    /*public void verwerkresultaat*/
     public void verwerkResultaat(char ch) {
         if (ch == 'w')
             aantalGewonnen = aantalGewonnen + 1;
@@ -26,6 +30,7 @@ public class Voetbalclub {
             aantalVerloren = aantalVerloren + 1;
     }
 
+    /*public string met return, door deze functie krijg je return club te zien met de gespeelde wedstrijden etc*/
     public String toString() {
         return club + "\t" + aantalGespeeld() + " " + aantalGewonnen + " " + aantalGelijk + " " + aantalVerloren + " " + aantalPunten();
     }
